6935ab2d968fa168c83cc1a7480292149c4d4e74
pawelplodzpl <pawel.kapusta@p.lodz.pl>
2019-12-16 09:20:32 +0100

    sample refactorization (python)

--- single_ball_follower/single_ball_follower.py
+++ single_ball_follower/single_ball_follower.py
@@ -1,11 +1,13 @@
 # parts of the code are based on https://www.pyimagesearch.com/2016/01/04/unifying-picamera-and-cv2-videocapture-into-a-single-class-with-opencv/
-# before running the code install imutils for python3 (pip3 install imutils)
+# before running the code install imutils for python3 (from https://github.com/pawelplodzpl/imutils)
 
+import sys
 import time
+import imutils
+from imutils.video import JetsonVideoStream
 # from imutils.video import VideoStream
 # import serial
-# from picamera.array import PiRGBArray
-# from picamera import PiCamera
+
 import numpy as np
 import cv2
 
@@ -17,119 +19,140 @@ def translate(value, oldMin, oldMax, newMin=-100, newMax=100):
     NewValue = (((value - oldMin) * newRange) / oldRange) + newMin
     return int(NewValue)
 
-usesPiCamera = False
 
-# camera = PiCamera()
-# camera.framerate = 60
-cameraResolution = (640, 480)
-# camera.resolution = cameraResolution
 
-# # camera.awb_mode = 'tungsten'
-# camera.vflip = camera.hflip = True
-# camera.video_stabilization = True
-# rawCapture = PiRGBArray(camera, size=cameraResolution)
+def getContours(image, mode=cv2.RETR_EXTERNAL):
+    contours = cv2.findContours(image, mode, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+    
+    return contours
 
-# initialize the video stream and allow the cammera sensor to warmup
-# vs = VideoStream(usePiCamera=usesPiCamera, resolution=cameraResolution, framerate=60).start()
-cap = cv2.VideoCapture('nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3264, height=1848, format=(string)NV12, framerate=28/1 ! nvvidconv flip-method=0 ! video/x-raw, width=960, height=460, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink' , cv2.CAP_GSTREAMER)
+def preprocessMask(mask, iterations):
+    kernel = np.ones((3,3),np.uint8)
+    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+    mask = cv2.dilate(mask, kernel, iterations=iterations)
+    mask = cv2.erode(mask, kernel, iterations=iterations)
+    # mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=iterations)
+    # mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=iterations)
 
-time.sleep(2.0)
+    return mask
+
+def getBoundingBoxes(contours, minArea):
+    boundingBoxes = []
+    
+    if contours:
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)
+        
+        for i, contour in enumerate(contours):
+            area = cv2.contourArea(contour)
+            if area > (minArea):
+                x,y,w,h = cv2.boundingRect(contour)
+                boundingBoxes.append((x,y,w,h))
+    
+    return boundingBoxes
+
+
+
+######## Main variables
+
+frameResolution = (960, 460)
+
+HorizontalFOV = 62
+VerticalFOV = 37
+
 
 colorLower = (0, 100, 50)
 colorUpper = (100, 255, 255)
 colorTolerance = 3
-paused = False
-roiSize = (16, 16) # roi size on the scaled down image (converted to HSV)
+roiSize = (frameResolution[0]//16, frameResolution[0]//16) # Region of Interest size
 
+
+usesPiCamera = False
+
+paused = False
+
+######## Initialization
+
+# vs = VideoStream(usePiCamera=usesPiCamera, resolution=cameraResolution, framerate=60).start()
+vs = JetsonVideoStream(outputResolution=frameResolution, gain=5.000)
+
+vs.start()
 
 # # initialize serial communication
 # ser = serial.Serial(port='/dev/ttyACM0', baudrate=57600, timeout=0.05)
 
+time.sleep(2.0)
+
+
+######## Main loop
+
 while True:
-# for cameraFrame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     loopStart = time.time()
     if not paused:
 
-        ret_val, frame = cap.read()
-        # frame = cv2.flip(frame, flipCode=-1)
-        
+        frame = vs.read()     
         height, width = frame.shape[0:2]
-        scaleFactor = 1
-        newWidth, newHeight = width//scaleFactor, height//scaleFactor
 
+        blurred = cv2.GaussianBlur(frame, (5, 5), 0)
 
-        resizedColor = cv2.resize(frame, (newWidth, newHeight), interpolation=cv2.INTER_CUBIC)
-        resizedColor_blurred = cv2.GaussianBlur(resizedColor, (5, 5), 0)
+        frame_HSV = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-        # resizedHSV = cv2.cvtColor(resizedColor, cv2.COLOR_BGR2HSV)
-        resizedHSV = cv2.cvtColor(resizedColor_blurred, cv2.COLOR_BGR2HSV)
-
-        roi = resizedHSV[newHeight//2 - roiSize[0]//2 : newHeight //2 + roiSize[0]//2, newWidth//2 - roiSize[1]//2 : newWidth//2 + roiSize[1]//2, :]
-        # roi = resizedHSV[10*newHeight//20 : 12*newHeight//20, 10*newWidth//20 : 12*newWidth // 20, :]
+        roi = frame_HSV[height//2 - roiSize[0]//2 : height //2 + roiSize[0]//2, width//2 - roiSize[1]//2 : width//2 + roiSize[1]//2, :]
         
         colorLowerWithTolerance = (colorLower[0] - colorTolerance,) + colorLower[1:]
         colorUpperWithTolerance = (colorUpper[0] + colorTolerance,) + colorUpper[1:]
 
-        mask = cv2.inRange(resizedHSV, colorLowerWithTolerance, colorUpperWithTolerance)
-        cv2.erode(mask, None, iterations=5)
-        cv2.dilate(mask, None, iterations=5)
-
-        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        boundingBoxes = []
-        biggestObject_BoundingBox = None
-        biggestObjectMiddle = None
-        if contours:
-            largestContour = max(contours, key=cv2.contourArea)
-            biggestObject_BoundingBox = cv2.boundingRect(largestContour)
-            
-            for i, contour in enumerate(contours):
-                area = cv2.contourArea(contour)
-                if area > ((newWidth * newHeight)/256):
-                    x,y,w,h = cv2.boundingRect(contour)
-                    boundingBoxes.append((x,y,w,h))
-                    # print("Object {}: ({},{}); {}x{}; area: {}".format(i, x,y,w,h, area))
-        else:
-            pass
-
-        upscaledColor = cv2.resize(resizedColor, (width, height), interpolation=cv2.INTER_NEAREST)
-        # draw ROI on upscaled image
-        xROI, yROI = width//2 - roiSize[1]//2 * scaleFactor, height//2 - roiSize[0]//2 * scaleFactor
-        cv2.rectangle(upscaledColor, (xROI, yROI), (xROI + roiSize[0]*scaleFactor, yROI + roiSize[1]*scaleFactor), (0, 0, 0), thickness=3)
-
-        for boundingBox in boundingBoxes:
-            x,y,w,h = boundingBox
-            cv2.rectangle(resizedColor, (x, y), (x+w, y+h), (255, 255, 0), thickness=1)
-            cv2.rectangle(upscaledColor, (x*scaleFactor, y*scaleFactor),
-                        ((x+w)*scaleFactor, (y+h)*scaleFactor), (255, 255, 0), thickness=2)
+        mask = cv2.inRange(frame_HSV, colorLowerWithTolerance, colorUpperWithTolerance)
+        # mask = preprocessMask(mask, 5)
         
-        if biggestObject_BoundingBox:
-            x, y, w, h = biggestObject_BoundingBox
-            biggestObjectMiddle = ((x+ w//2)*scaleFactor, (y + h//2)*scaleFactor)
-            cv2.rectangle(resizedColor, (x, y), (x+w, y+h), (0, 0, 255), thickness=2)
-            cv2.rectangle(upscaledColor, (x*scaleFactor, y*scaleFactor),
-                            ((x+w)*scaleFactor, (y+h)*scaleFactor), (0, 0, 255), thickness=3)
-            cv2.circle(upscaledColor, biggestObjectMiddle, 2, (255, 0, 0), thickness=2)
-            screenMiddle = width//2, height//2
-            distanceVector = tuple(map(lambda x, y: x - y, biggestObjectMiddle, screenMiddle))
-            # print("Vector: {}".format(distanceVector))
-            scaled = (translate(distanceVector[0], -width//2, width//2), translate(distanceVector[1], -height//2, height//2) )
-            # print("Vector scaled: {}".format(scaled))
-            pitch = scaled[1] # up-down
-            yaw = scaled[0] # left-right
-            cv2.line(upscaledColor, screenMiddle, biggestObjectMiddle, (0, 0, 255))
-            # packet = '<packet, {}, {}>'.format(yaw, pitch)
-            # packetBytes = bytes(packet, 'utf-8')
-            
-            # ser.write(packetBytes)
-            # print(ser.read_all())
-            
+        # get contours of objects in image
+        contours = getContours(mask)
 
-        cv2.imshow("video", upscaledColor)
+
+        # get bounding boxes for contrours
+        minimalContourArea = (width * height)/256
+        boundingBoxes = getBoundingBoxes(contours, minimalContourArea)
+        
+
+
+        # draw ROI on image
+        xROI, yROI = width//2 - roiSize[1]//2 , height//2 - roiSize[0]//2
+        cv2.rectangle(frame, (xROI, yROI), (xROI + roiSize[0], yROI + roiSize[1]), (0, 0, 0), thickness=3)
+
+
+        # draw bounding boxes, find largest object
+        for i, boundingBox in enumerate(boundingBoxes):
+            
+            x, y, w, h = boundingBox
+
+            if i is 0:
+                biggestObjectMidPoint = ((x+ w//2), (y + h//2))
+                cv2.rectangle(frame, (x, y), ((x+w), (y+h)), (0, 0, 255), thickness=3)
+                cv2.circle(frame, biggestObjectMidPoint, 4, (255, 0, 0), thickness=3)
+
+                screenMidPoint = width//2, height//2
+                distanceVector = tuple(map(lambda x, y: x - y, biggestObjectMidPoint, screenMidPoint))
+                # print("Vector: {}".format(distanceVector))
+                yaw = translate(distanceVector[0], -width//2, width//2, -HorizontalFOV//2, HorizontalFOV//2) # up-down
+                pitch = translate(distanceVector[1], -height//2, height//2, -VerticalFOV//2, VerticalFOV//2) # left-right
+                
+                print("Yaw: {} deg, Pitch: {} deg\n".format(yaw, pitch))
+                
+                
+                cv2.line(frame, screenMidPoint, biggestObjectMidPoint, (0, 0, 255))
+                # packet = '<servo, {}, {}>'.format(yaw, pitch)
+                # packetBytes = bytes(packet, 'utf-8')
+                
+                # ser.write(packetBytes)
+                # print(ser.read_all())
+            else:
+                cv2.rectangle(frame, (x, y), ((x+w), (y+h)), (255, 255, 0), thickness=2)
+                 
+
+        cv2.imshow("video", frame)
         cv2.imshow("roi", roi)
         cv2.imshow("mask", mask)
 
-        modTolerances = False
 
     # handle keys 
     key = cv2.waitKey(1) & 0xFF
@@ -168,13 +191,23 @@ while True:
     elif key == ord('p'):
         paused = not paused
     elif key == ord('d'):
-        # pause/unpause arduino camera movement
-        ser.write(bytes('d', 'utf-8'))
-    
-    # rawCapture.truncate(0)
-    loopEnd= time.time()
+        # pause/unpause camera movement
+        packet = '<stop, 0, 0>'
+        packetBytes = bytes(packet, 'utf-8')  
+        # ser.write(packetBytes)
+
+    loopEnd = time.time()
     print("loop execution took {:3.2f}ms".format((loopEnd - loopStart)*1000))
     
+
+    
 # cleanup
+
+# ser.close()
 cv2.destroyAllWindows()
+
+# to cleanly stop frame grabbing thread
 vs.stop()
+time.sleep(1.0)
+sys.exit(0)
+
